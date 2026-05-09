f7e0731ca0eb204fbdfe1b49d77b2b16c7d46219
ihavelapki <kotlapkovskij@gmail.com>
2026-05-09 16:31:50 +0300

    old backend version has been deleted

--- plotapp/src/app/api/v1/endpoints/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.analysis.analysis import read_log_files
-
-router = APIRouter()
-
-@router.get("/metrics")
-def get_metrics():
-    return read_log_files("../var/log", "csv", 8600)
